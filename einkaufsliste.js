86451b964d4ba9ea2237c101dfd2869cc51ac3a7
fragmentscode <sacha84@bluewin.ch>
2026-07-03 11:40:37 +0200

    Add files via upload

---
+++ einkaufsliste.js
@@ -0,0 +1,6 @@
+const eingabefeld = document.getElementById("eingabe");
+const knopf = document.getElementById("hinzufuegen");
+
+knopf.addEventListener("click", function () {
+  alert("Der Knopf funktioniert!");
+});
